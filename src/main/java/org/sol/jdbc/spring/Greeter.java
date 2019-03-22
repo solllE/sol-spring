8268fccf047878b1bacbc6093cdf015e3c9342e8
sollle <qufrhdwn1111@naver.com>
2019-03-22 18:24:33 +0900

    소스 추가

---
+++ src/main/java/org/sol/jdbc/spring/Greeter.java
@@ -0,0 +1,12 @@
+package org.sol.jdbc.spring;
+
+public class Greeter {
+	
+	public Greeter() {
+		System.out.println("Greeter instance created!");
+	}
+	
+	public String greet() {
+		return "Hello, Spring!";
+	}
+}
