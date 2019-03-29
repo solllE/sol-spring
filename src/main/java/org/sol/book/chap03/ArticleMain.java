ecde4a4ae13a24c0edf098caf204e52f76dc44bb
sollle <qufrhdwn1111@naver.com>
2019-03-29 10:33:11 +0900

    chap03 추가

---
+++ src/main/java/org/sol/book/chap03/ArticleMain.java
@@ -0,0 +1,19 @@
+package org.sol.book.chap03;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ArticleMain {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext("chap02.xml");
+		ArticleService articleService = ctx.getBean("articleService", org.sol.book.chap03.ArticleService.class);
+		MemberService memberService = ctx.getBean("memberService", org.sol.book.chap03.MemberService.class);
+
+		articleService.addArticle();
+		memberService.registerMember();
+
+		ctx.close();
+	}
+
+}
